6502625658e2f4d94c98f14f762ac82bc0a67108
Adam Cipkala <acipkala@gmail.com>
2024-03-13 23:30:00 +0100

    ProductRatings, generate migration for ProductRatings

--- SchemaMigration/SchemaMigration/ApplicationDbContext.cs
+++ SchemaMigration/SchemaMigration/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<ProductRating> ProductRatings { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
---
+++ SchemaMigration/SchemaMigration/Models/ProductRating.cs
@@ -0,0 +1,11 @@
+﻿namespace SchemaMigration.Models
+{
+    public class ProductRating
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Rating { get; set; }
+    }
+
+}
